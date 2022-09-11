e35f13a1e69e7b6c9dd5f142ca2d4d67d279d13b
mgenius3 <benmos16@gmail.com>
2022-09-11 00:22:29 -0700

    cap readme

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ */
+int main(void)
+{
+	fprintf(stderr, "%s", "and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return(1);
+}
