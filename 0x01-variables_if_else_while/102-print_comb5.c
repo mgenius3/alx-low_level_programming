801ea668904ddef920aa293bcb630df663ce2e26
Moses Benjamin <52639696+mgenius3@users.noreply.github.com>
2022-09-12 00:16:06 -0700

    Update 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * codes for printing all possible combination of two digits
+ * in ascending order
+ *
+ * Return 0 success
+ */
+ int main (void)
+ {
+ return (0)
+ }
