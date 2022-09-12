4628a2f1b76326306cef75d5833ed49f1f894da3
Moses Benjamin <52639696+mgenius3@users.noreply.github.com>
2022-09-12 00:04:40 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	int digit1, digit2, digit3;
 
-	for (digit1 = digit1 + 1; digit1 + 1; digit1 < 10; digit1++)
+	for (digit1 = digit1 + 1; digit1 < 10; digit1++)
 	{
 		for (digit2 = digit1 + 1; digit2 < 10; digit3++)
 		{
