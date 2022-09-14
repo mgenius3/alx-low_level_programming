493e1a9478656acbd246c80de8caf9d520bd4af5
Moses Benjamin <52639696+mgenius3@users.noreply.github.com>
2022-09-13 20:41:20 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,19 +9,21 @@ void times_table(void)
 
 	for (num = 0; num <= 9; num++)
 	{
+		putchar('0');
+		
 		for (mult = 1; mult <= 9; mult++)
 		{	
-		_putchar(',');
-		_putchar(' ');
-
-		prod = num * mult;
-
-		if (prod <= 9)
+			_putchar(',');
 			_putchar(' ');
-		else
-			_putchar((prod /10) + 'a');
 
-		_putchar((prod % 10) + '0');
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod /10) + '0');
+
+			_putchar((prod % 10) + '0');
 	}
 	_putchar('\n');
 }
