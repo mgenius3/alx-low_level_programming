74f8178ec1990ed226b8c6ffff781a3ec4dd8287
Moses Benjamin <52639696+mgenius3@users.noreply.github.com>
2022-09-22 17:25:44 -0700

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -10,9 +10,9 @@
 char *_strncat(char *dest, char*src, int n)
 {
       int index=0,dest_len=0;
-      while(dest[index ++])
+      while (dest[index ++])
          dest_len ++;
-      for(index= 0; src[index] && index< n; index ++)
+      for (index= 0; src[index] && index< n; index ++)
          dest[dest_len ++]=src[index];
       return(dest);
 }
